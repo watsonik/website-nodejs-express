626d79c9af0a4538a2584de68ce5eae667498de1
Ihar Sakun <watsonik@gmail.com>
2021-12-01 02:56:04 +0300

    Add Cookies

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ const router = express.Router();
 module.exports = params => {
 
     router.get('/', async (request, response) => {
+
+        if (!request.session.visitcount) {
+            request.session.visitcount = 0;
+        }
+        request.session.visitcount += 1;
+        console.log(`Number of visits: ${request.session.visitcount}`);
+
         response.render('pages/index', { pageTitle: 'Welcome' });
     });
 
