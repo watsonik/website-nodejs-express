0057caf553ca05eb0b0853ebce622b68f698deeb
Ihar Sakun <watsonik@gmail.com>
2021-12-01 02:00:06 +0300

    Add routes for subpages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const spaekersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
 const router = express.Router();
 
 module.exports = () => {
     router.get('/', (request, response) => {
         response.render('pages/index', { pageTitle: 'Welcome' });
     });
+
+    router.use('/speakers', spaekersRoute());
+    router.use('/feedback', feedbackRoute());
+    
     return router;
 }
