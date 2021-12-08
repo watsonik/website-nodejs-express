2abb3cfd66c9832f110fd41e5918f51ab512e7f5
Ihar Sakun <watsonik@gmail.com>
2021-12-09 02:24:31 +0300

    Add layouts

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,7 @@ const router = express.Router();
 module.exports = params => {
 
     router.get('/', async (request, response) => {
-
-        if (!request.session.visitcount) {
-            request.session.visitcount = 0;
-        }
-        request.session.visitcount += 1;
-        console.log(`Number of visits: ${request.session.visitcount}`);
-
-        response.render('pages/index', { pageTitle: 'Welcome' });
+        response.render('layout', { pageTitle: 'Welcome', template:'index' });
     });
 
     router.use('/speakers', spaekersRoute(params));
