0436d525f2b84ff00c3224648e8bff8eb877cc69
Ihar Sakun <watsonik@gmail.com>
2021-12-15 16:43:12 +0300

    Create API endpoint

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,6 +3,29 @@ const express = require('express');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
+const validations = [
+    check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A name is requierd'),
+    check('email')
+        .trim()
+        .isEmail({})
+        .normalizeEmail()
+        .withMessage('A valid email address is requierd'),
+    check('title')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A title is requierd'),
+    check('message')
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage('A message is requierd'),
+
+];
 
 module.exports = (params) => {
     const { feedbackService } = params;
@@ -22,46 +45,44 @@ module.exports = (params) => {
 
     });
 
-    router.post('/', [
-        check('name')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A name is requierd'),
-        check('email')
-            .trim()
-            .isEmail({})
-            .normalizeEmail()
-            .withMessage('A valid email address is requierd'),
-        check('title')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A title is requierd'),
-        check('message')
-            .trim()
-            .isLength({ min: 5 })
-            .escape()
-            .withMessage('A message is requierd'),
+    router.post('/', validations,
+        async (request, response, next) => {
+            try {
+                const errors = validationResult(request);
+                if (!errors.isEmpty()) {
+                    request.session.feedback = {
+                        errors: errors.array(),
+                    };
+                    return response.redirect('/feedback');
+                }
 
-    ],
-        async (request, response) => {
+                const { name, email, title, message } = request.body;
+                await feedbackService.addEntry(name, email, title, message);
+                request.session.feedback = {
+                    message: 'Thank you for your feedback',
+                }
+
+                return response.redirect('/feedback');
+            } catch (error) {
+                return next(error);
+            }
+
+        });
+
+    router.post('/api', validations, async (request, response, next) => {
+        try {
             const errors = validationResult(request);
             if (!errors.isEmpty()) {
-                request.session.feedback = {
-                    errors: errors.array(),
-                };
-                return response.redirect('/feedback');
+                return response.json({errors:errors.array()});
             }
-
             const { name, email, title, message } = request.body;
             await feedbackService.addEntry(name, email, title, message);
-            request.session.feedback = {
-                message: 'Thank you for your feedback',
-            }
-
-            return response.redirect('/feedback');
-        });
+            const feedback = await feedbackService.getList();
+            return response.json({ feedback });
+        } catch (error) {
+            return next(error);
+        }
+    });
 
     return router;
 };
--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(cookieSession({
 }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
