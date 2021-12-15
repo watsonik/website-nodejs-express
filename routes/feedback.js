64221aa81872324cf7bcd9227c04e6fe3be8e3d3
Ihar Sakun <watsonik@gmail.com>
2021-12-15 03:02:35 +0300

    Validate and sanitize user's imput

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 module.exports = (params) => {
@@ -8,15 +10,47 @@ module.exports = (params) => {
     router.get('/', async (request, response, next) => {
         try {
             const feedback = await feedbackService.getList();
-            return response.render('layout', { pageTitle: 'Feedback', template: 'feedback',feedback });
+
+            const errors = request.session.feedback ? request.session.feedback.errors : false;
+            request.session.feedback = {};
+
+            return response.render('layout', { pageTitle: 'Feedback', template: 'feedback', feedback, errors });
         } catch (error) {
             return next(error);
         }
 
     });
 
-    router.post('/', (request, response) => {
-        console.log(request.body);
+    router.post('/', [
+        check('name')
+            .trim()
+            .isLength({ min: 3 })
+            .escape()
+            .withMessage('A name is requierd'),
+        check('email')
+            .trim()
+            .isEmail({})
+            .normalizeEmail()
+            .withMessage('A valid email address is requierd'),
+        check('title')
+            .trim()
+            .isLength({ min: 3 })
+            .escape()
+            .withMessage('A title is requierd'),
+        check('message')
+            .trim()
+            .isLength({ min: 5 })
+            .escape()
+            .withMessage('A message is requierd'),
+
+    ], (request, response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            request.session.feedback = {
+                errors: errors.array(),
+            };
+            return response.redirect('/feedback');
+        }
         return response.send('Feedback form posted');
     });
 
